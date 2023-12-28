03daaaefa8b0a5d54795d40888150024c9b550c5
SaintCavvy <saintcavvy@gmail.com>
2023-12-28 20:16:57 +0200

    added appsettings, fluent migrator etc.

--- Shop.Main/Program.cs
+++ Shop.Main/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace Shop
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using FluentMigrator.Runner;
+using System.Reflection;
+
+
+
+namespace Shop
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static IConfigurationRoot config;
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            using (var connection = new SqlConnection(config.GetConnectionString("MyShop")))
+            {
+
+            }
         }
+
+        public static IServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                .AddLogging(c => c.AddFluentMigratorConsole())
+                .AddFluentMigratorCore()
+                .ConfigureRunner(c => c.AddSqlServer2012()
+                       .WithGlobalConnectionString(config.GetConnectionString("MyShop"))
+                       .ScanIn(Assembly.GetExecutingAssembly()).For.Migrations())
+                .BuildServiceProvider(false);
+        }
+
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
+        }
+
+
+        public static void Initconfig()
+        {
+            var builder = new ConfigurationBuilder();
+            builder
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            config = builder.Build();
+        }
+
     }
 }
